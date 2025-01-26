ecfccc8a0b38d32740d25bbd4394693bb3807e11
xavier-p-snhu <xavier.paquette@snhu.edu>
2025-01-25 20:07:54 -0500

    Initial Commit

--- CS 300 Project Two/CS 300 Project Two/Main.cpp
+++ CS 300 Project Two/CS 300 Project Two/Main.cpp
@@ -1,11 +1,3 @@
-//============================================================================
-// Name        : BinarySearchTree.cpp
-// Author      : JYour name
-// Version     : 1.0
-// Copyright   : Copyright © 2017 SNHU COCE
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <time.h>
 #include <fstream>
@@ -20,10 +12,14 @@ using std::vector;
 // Global definitions visible to all methods and classes
 //============================================================================
 
-int DEFAULT_SIZE = 4;
+enum Color
+{
+    RED, BLACK
+};
 
 // forward declarations
 double strToDouble(string str, char ch);
+string colorToString(Color color);
 
 // define a structure to hold course information
 struct Course {
@@ -34,19 +30,23 @@ struct Course {
     }
 };
 
-class HashTable {
+class RedBlackTree {
 
 private:
     // Define structures to hold courses
     struct Node {
         Course course;
         unsigned int key;
-        Node* next;
+        Color nodeColor;
+        Node * left, * right, * parent;
 
         // default constructor
         Node() {
             key = UINT_MAX;
-            next = nullptr;
+            left = nullptr;
+            right = nullptr;
+            parent = nullptr;
+            nodeColor = Color::RED;
         }
 
         // initialize with a course
@@ -60,52 +60,165 @@ private:
         }
     };
 
-    vector<Node> nodes;
+    Node* root;
 
-    unsigned int tableSize = DEFAULT_SIZE;
+    Node* Grandparent(Node* n) {
+        if (n->parent != nullptr) {
+            return n->parent;
+        }
+        return nullptr;
+    }
 
-    unsigned int hash(int key);
+    Node* Uncle(Node* n) {
+        Node* g = Grandparent(n);
+        if (g->parent == NULL) {
+            return nullptr;
+        }
+        else if (g->left == n->parent) {
+            return g->right;
+        }
+        else {
+            return g->left;
+        }
+        return nullptr;
+    }
+
+    Node* Sibling(Node* n) {
+        if (n == nullptr || n->parent == nullptr) {
+            return nullptr;
+        }
+        if (n == n->parent->left) {
+            return n->parent->right;
+        }
+        else {
+            return n->parent->left;
+        }
+        return nullptr;
+    }
+
+    void RotateLeft(Node* n) {
+        Node* r = n->right;
+        if (r == nullptr) {
+            return; // Cannot rotate left if there is no right child
+        }
+
+        n->right = r->left;
+        if (r->left != nullptr) {
+            r->left->parent = n;
+        }
+
+        r->parent = n->parent;
+        if (n->parent == nullptr) {
+            root = r; // Update the root if n was the root
+        }
+        else if (n == n->parent->left) {
+            n->parent->left = r;
+        }
+        else {
+            n->parent->right = r;
+        }
+
+        r->left = n;
+        n->parent = r;
+    }
+
+
+    void RotateRight(Node* n) {
+        Node* l = n->left;
+        if (l == nullptr) {
+            return; // Cannot rotate right if there is no left child
+        }
+
+        n->left = l->right;
+        if (l->right != nullptr) {
+            l->right->parent = n;
+        }
+
+        l->parent = n->parent;
+        if (n->parent == nullptr) {
+            root = l; // Update the root if n was the root
+        }
+        else if (n == n->parent->left) {
+            n->parent->left = l;
+        }
+        else {
+            n->parent->right = l;
+        }
+
+        l->right = n;
+        n->parent = l;
+    }
+
+    void InsertFixup(Node* n) {
+        while (n != root && n->parent->nodeColor == Color::RED) {
+            Node* u = Uncle(n);
+            if (u != nullptr && n->parent->nodeColor == Color::RED) {
+                //Case 1: uncle is red
+                n->parent->nodeColor = Color::BLACK;
+                u->nodeColor = Color::BLACK;
+                Grandparent(n)->nodeColor = Color::BLACK;
+                n = Grandparent(n);
+            }
+            else {
+                if (n->parent == Grandparent(n)->left) {
+                    //Uncle is black, parent is left child
+                    if (n == n->parent->right) {
+                        n = n->parent;
+                        RotateLeft(n);
+                    }
+                    n->parent->nodeColor = Color::BLACK;
+                    Grandparent(n)->nodeColor = Color::RED;
+                    RotateLeft(Grandparent(n));
+                }
+                else {
+                    //Uncle is black, parent is right child
+                    if (n == n->parent->left) {
+                        n = n->parent;
+                        RotateRight(n);
+                    }
+                    n->parent->nodeColor = Color::BLACK;
+                    Grandparent(n)->nodeColor = Color::RED;
+                    RotateLeft(Grandparent(n));
+                }
+            }
+        }
+        root->nodeColor = Color::BLACK;
+    }
+
+    void InOrderTraversal(Node* n, int depth = 0) {
+        if (n == nullptr) {
+            return;
+        }
+        InOrderTraversal(n->left);
+        std::cout << n->key << " | " << n->course.title << " " << colorToString(n->nodeColor) << endl;
+        InOrderTraversal(n->right);
+    }
 
 public:
-    HashTable();
-    HashTable(unsigned int size);
-    virtual ~HashTable();
-    void Insert(Course course);
-    void PrintAll();
-    void PrintAllAlphanumeric();
+    RedBlackTree();
+    void Insert(Course course, int key);
+    virtual ~RedBlackTree();
     Course Search(string courseId);
+    void printInOrderTraversal();
 };
 
 /**
  * Default constructor
  */
-HashTable::HashTable() {
+RedBlackTree::RedBlackTree() {
     // FIXME (1): Initialize the structures used to hold courses
-
-    // Initalize node structure by resizing tableSize
-    nodes.resize(tableSize);
+    // FIXME (1.5) Am I supposed to put something here?
 }
-
-/**
- * Constructor for specifying size of the table
- * Use to improve efficiency of hashing algorithm
- * by reducing collisions without wasting memory.
- */
-HashTable::HashTable(unsigned int size) {
-    // invoke local tableSize to size with this->
-    tableSize = size;
-    // resize nodes size
-    nodes.resize(tableSize);
-}
-
 
 /**
  * Destructor
  */
-HashTable::~HashTable() {
+RedBlackTree::~RedBlackTree() {
     // FIXME (2): Implement logic to free storage when class is destroyed
 
     //loop through each node in the vector
+
+    /*
     for (unsigned int i = 0; i < nodes.size(); i++) {
         Node* current = &nodes[i];
         //loop through the linked list of nodes
@@ -115,98 +228,36 @@ HashTable::~HashTable() {
             current = nextNode;
         }
     }
+    */
 }
 
-/**
- * Calculate the hash value of a given key.
- * Note that key is specifically defined as
- * unsigned int to prevent undefined results
- * of a negative list index.
- *
- * @param key The key to hash
- * @return The calculated hash
- */
-unsigned int HashTable::hash(int key) {
-    // FIXME (3): Implement logic to calculate a hash value
+//FIXME do the comment thing like above deconstructor
+void RedBlackTree::Insert(Course course, int key) {
+    Node* new_node = new Node(course, key);
+    Node* current = root;
+    Node* parent = nullptr;
 
-    //returns key modulus tableSize
-    //returns a value from 0 to tableSize - 1
-    return key % tableSize;
-}
-
-/**
- * Insert a course
- *
- * @param course The course to insert
- */
-void HashTable::Insert(Course course) {
-    // FIXME (5): Implement logic to insert a course
-    // create the key for the given course
-    int courseKey = hash(int(course.courseId.at(4)));
-    // retrieve node using key
-    Node* node = &nodes[courseKey];
-
-    // if no entry found for the key
-    if (node->key == UINT_MAX) {
-        // assign the course to the node
-        node->course = course;
+    //Find the correct position to insert the node
+    while (current != nullptr) {
+        parent = current;
+        if (new_node->key < current->key) {
+            current = current->left;
+        }
+        else {
+            current = current->right;
+        }
     }
-    // else find the next open node
+
+    //Insert the node
+    new_node->parent = parent;
+    if (parent == nullptr) {
+        root = new_node;
+    }
+    else if (new_node->key < parent->key) {
+        parent->left = new_node;
+    }
     else {
-        while (node->next != nullptr)
-        {
-            node = node->next;
-        }
-        // add new newNode to end
-        node->next = new Node(course);
-    }
-
-    //make sure node has the key
-    node->key = courseKey;
-}
-
-void HashTable::PrintAll()
-{
-    // for node begin to end iterate
-    for (int i = 0; i < nodes.size(); i++) {
-
-        cout << i << ":";
-        //   if key not equal to UINT_MAx
-        if (nodes[i].key != UINT_MAX) {
-            Node* node = &nodes[i];
-            // while node not equal to nullptr
-            while (node != nullptr) {
-                // output key, courseID, title, amount and fund
-                Course course = node->course;
-                cout << course.courseId << " ";
-                // node is equal to next node
-                node = node->next;
-            }
-        }
-        cout << endl;
-    }
-}
-
-void HashTable::PrintAllAlphanumeric()
-{
-    vector<string> courses;
-    // for node begin to end iterate
-    for (int i = 0; i < nodes.size(); i++) {
-        //   if key not equal to UINT_MAx
-        if (nodes[i].key != UINT_MAX) {
-            Node* node = &nodes[i];
-            // while node not equal to nullptr
-            while (node != nullptr) {
-                courses.push_back(node->course.courseId);
-                // node is equal to next node
-                node = node->next;
-            }
-        }
-    }
-    sort(courses.begin(), courses.end());
-    
-    for (int i = 0; i < courses.size(); i++) {
-        cout << courses[i] << endl;
+        parent->right = new_node;
     }
 }
 
@@ -215,9 +266,10 @@ void HashTable::PrintAllAlphanumeric()
  *
  * @param courseId The course id to search for
  */
-Course HashTable::Search(string courseId) {
+Course RedBlackTree::Search(string courseId) {
+    //FIXME rework for hashtable
     Course course;
-
+    /*
     // create the key for the given course
     int hashKey = hash(int(courseId.at(4)));
     // if entry found for the key
@@ -235,8 +287,14 @@ Course HashTable::Search(string courseId) {
             searchingNode = searchingNode->next;
         }
     }
+    */
 
     return course;
+}
+
+void RedBlackTree::printInOrderTraversal()
+{
+    InOrderTraversal(root);
 }
 
 /**
@@ -263,7 +321,7 @@ void displayCourse(Course course) {
  * @param csvPath the path to the CSV file to load
  * @return a container holding all the courses read
  */
-void loadcourses(string filePath, HashTable* bst) {
+void loadcourses(string filePath, RedBlackTree* bst) {
     cout << "Loading file " << filePath << endl;
 
     ifstream file (filePath);
@@ -287,7 +345,8 @@ void loadcourses(string filePath, HashTable* bst) {
             for (int i = 2; i < vec.size(); i++) {
                 course.prereqs.push_back(vec[i]);
             }
-            bst->Insert(course);
+
+            bst->Insert(course, stoi(course.courseId.substr(vec[0].length() - 3)));
 
         }
         file.close();
@@ -309,6 +368,19 @@ double strToDouble(string str, char ch) {
     return atof(str.c_str());
 }
 
+string colorToString(Color color)
+{
+    switch (color)
+    {
+    case RED:
+        return "RED";
+        break;
+    case BLACK:
+        return "BLACK";
+        break;
+    }
+}
+
 /**
  * The one and only main() method
  */
@@ -316,14 +388,14 @@ int main(int argc, char* argv[]) {
 
     string csvPath, courseKey;
     csvPath = "courses.txt";
-    courseKey = "CSCI400";
+    courseKey = "400";
 
     // Define a timer variable
     clock_t ticks;
 
-    HashTable* ht;
+    RedBlackTree* rbt;
     Course course;
-    ht = new HashTable;
+    rbt = new RedBlackTree;
 
     int choice = 0;
     while (choice != 9) {
@@ -344,7 +416,7 @@ int main(int argc, char* argv[]) {
             ticks = clock();
 
             // Complete the method call to load the courses
-            loadcourses(csvPath, ht);
+            loadcourses(csvPath, rbt);
 
             // Calculate elapsed time and display result
             ticks = clock() - ticks; // current clock ticks minus starting clock ticks
@@ -352,15 +424,17 @@ int main(int argc, char* argv[]) {
             cout << "time: " << ticks * 1.0 / CLOCKS_PER_SEC << " seconds" << endl;
             break;
         case 2:
-            ht->PrintAll();
+            rbt->printInOrderTraversal();
             break;
         case 3:
-            ht->PrintAllAlphanumeric();
+            //FIXME: remove case 3, or change it
+            cout << "Not implemented :(" << endl;
+            //rbt->PrintAllAlphanumeric();
             break;
         case 4:
             ticks = clock();
 
-            course = ht->Search(courseKey);
+            course = rbt->Search(courseKey);
 
             ticks = clock() - ticks; // current clock ticks minus starting clock ticks
 
